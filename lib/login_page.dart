8cb4022e8ab5b70b44841f7f8839de9c6095b38b
dhimweray222 <dhimweray222@gmail.com>
2022-05-30 08:55:56 +0700

    Projek TPM IF E (Dhimas Wahyu A/123190071)

---
+++ lib/login_page.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import '/models/home.dart';
+import 'package:recipe_app/models/firstpage.dart';
+
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String username = "";
+  String password = "";
+  bool isLoginSuccess = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Login Page"),
+        ),
+        body: Stack(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 10.0),
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child:
+              Image.asset(
+                'lib/models/images/263d53ed14ea33255b11d342a25c20d4.jpg',
+                fit: BoxFit.fill,
+              ),
+            ),
+            Center(
+            child: Column(
+              children: [
+                SizedBox(height: 200),
+                _usernameField(),
+                _passwordField(),
+                _loginButton(context),
+              ],
+            ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _usernameField() {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: TextFormField(
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.black,
+              fontSize: 20.0,
+          ),
+          onChanged: (value) {
+            username = value;
+          },
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white70,
+            hintStyle: TextStyle(fontSize: 20.0, color: Colors.black),
+            hintText: "Username",
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                borderSide: BorderSide(
+                    color: (isLoginSuccess) ? Colors.blue : Colors.red)),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                borderSide: BorderSide(color: Colors.green)),
+          ),
+        ));
+  }
+
+  Widget _passwordField() {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: TextFormField(
+          textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.black
+            ),
+          onChanged: (value) {
+            password = value;
+          },
+          obscureText: true,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white70,
+            hintStyle: TextStyle(fontSize: 20.0, color: Colors.black),
+            hintText: "Password",
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                borderSide: BorderSide(
+                    color: (isLoginSuccess) ? Colors.blue : Colors.red)),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                borderSide: BorderSide(color: Colors.green)),
+          ),
+        ));
+  }
+
+  Widget _loginButton(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      width: 200.0,
+      height: 60.0,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: (isLoginSuccess) ? Colors.blue : Colors.red,
+        ),
+        onPressed: () {
+          String pesan = "";
+          if (password == "dhimas") {
+            setState(() {
+              pesan = "Login Success";
+              isLoginSuccess = true;
+            });
+            Navigator.push(context, MaterialPageRoute(builder: (context) => Firstpage()));
+          } else {
+            setState(() {
+              pesan = "Login Failed";
+              isLoginSuccess = false;
+            });
+          }
+          SnackBar snackBar = SnackBar(
+            content: Text(pesan),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+        child: Text('Login'),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/models/firstpage.dart';
+import 'package:recipe_app/login_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
+
